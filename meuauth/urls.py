fa611741cfe3ed9f857457c70f691a1c46e05623
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-08-18 20:25:33 -0300

    Será que conseguimos rodar o linter também de forma automatizada?

--- meuauth/urls.py
+++ meuauth/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from meuauth import views
 
 urlpatterns = [
-    path('signin/', views.sign_in, name='signin'),
+    path("signin/", views.sign_in, name="signin"),
     # https://magote.com/myauth/profile/129/
-    path('profile/<corno_id>/', views.perfil, name="perfil"),
-    path('signup/', views.cadastro, name='cadastro'),
+    path("profile/<corno_id>/", views.perfil, name="perfil"),
+    path("signup/", views.cadastro, name="cadastro"),
 ]
--- meuauth/views.py
+++ meuauth/views.py
@@ -7,38 +7,38 @@ from .models import CornoProfile
 
 # Create your views here.
 def sign_in(request):
-    if request.method == 'POST':
-        user = request.POST['user']
-        passwd = request.POST['passwd']
+    if request.method == "POST":
+        user = request.POST["user"]
+        passwd = request.POST["passwd"]
 
         user = authenticate(request, username=user, password=passwd)
 
         if user:
             login(request, user)
-            return HttpResponse('O login ocorreu.')
+            return HttpResponse("O login ocorreu.")
         else:
-            return HttpResponse('Você não sabe de nada.')
+            return HttpResponse("Você não sabe de nada.")
     else:
-        return render(request, 'meuauth/index.html')
+        return render(request, "meuauth/index.html")
 
 
 def perfil(request: HttpRequest, corno_id: str) -> HttpResponse:
-    '''Perfil de um corno.'''
-    return HttpResponse(f'Olá corno de ID {corno_id}')
+    """Perfil de um corno."""
+    return HttpResponse(f"Olá corno de ID {corno_id}")
 
 
 def cadastro(request):
-    nome = request.GET['nome']
-    senha = request.GET['senha']
-    tipo = request.GET['tipo']
+    nome = request.GET["nome"]
+    senha = request.GET["senha"]
+    tipo = request.GET["tipo"]
 
     novo_corno = CornoProfile()
     novo_corno.tipo = tipo
 
-    novo_usuario = User.objects.create_user(nome,'',senha)
+    novo_usuario = User.objects.create_user(nome, "", senha)
     novo_usuario.save()
 
     novo_corno.user = novo_usuario
     novo_corno.save()
 
-    return HttpResponse('Corno salvo com sucesso! Bem vindo ao mundo dos cornos.')
+    return HttpResponse("Corno salvo com sucesso! Bem vindo ao mundo dos cornos.")
