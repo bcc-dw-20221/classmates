4511d49baed2c03a01931e489fe99ff05b8eb386
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-09-01 21:36:18 -0300

    Verificando onde talvez estejam sendo utilizados settings.

--- magote/urls.py
+++ magote/urls.py
@@ -17,9 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 def teste(request):
     """Faz nada."""
@@ -30,4 +27,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("boteco/", include("boteco.urls")),
     path("", teste, name="home"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
