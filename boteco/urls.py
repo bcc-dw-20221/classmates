df9938c6f1e4ae79a62a2e611360e725ef7e2839
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-09-03 00:43:33 -0300

    Primeiros passos de uma API Rest.
    Instalação, configuração da extensão, serializer, viewset e
    router inicial configurados.

--- boteco/urls.py
+++ boteco/urls.py
@@ -1,11 +1,22 @@
 """URL paths do bar."""
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from boteco import views
+from boteco.viewsets import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path("", views.get_postagens, name="get_postagens"),
     path("add/", views.post_postagem, name="post_postagem"),
     path("get/<postagem_id>/", views.get_postagem, name="get_postagem"),
-    path("remove/<postagem_id>/", views.delete_postagem, name="delete_postagem"),
+    path(
+        "remove/<postagem_id>/", views.delete_postagem, name="delete_postagem"
+    ),
+    path("api/", include(router.urls)),
+    path(
+        "api-auth/", include("rest_framework.urls", namespace="rest_framework")
+    ),
 ]
--- magote/urls.py
+++ magote/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 def teste(request):
     """Faz nada."""
     return render(request, "magote/index.html")
