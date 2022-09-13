326c134e8aae4273c6d2c1bc58ab770b51a6a544
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-09-13 12:08:09 -0300

    O linter mandou.

--- boteco/serializers.py
+++ boteco/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
 from rest_framework.response import Response
 
 from boteco.models import Perfil, Bebida, TipoDeCorno
