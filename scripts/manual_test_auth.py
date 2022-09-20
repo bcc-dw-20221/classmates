4243fa334d49cc75a445d44891d5b0ac443b37eb
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-09-20 20:11:55 -0300

    Facilitando a vida
    Facilitando nossa vida nos testes.

--- scripts/manual_test_auth.py
+++ scripts/manual_test_auth.py
@@ -1,7 +1,15 @@
 import json
+from faker import Faker
+from faker.providers import date_time, person, internet, misc
+
 import requests as r
 
-from datetime import datetime
+Faker.seed(0)
+fake = Faker("pt-BR")
+fake.add_provider(date_time)
+fake.add_provider(person)
+fake.add_provider(internet)
+fake.add_provider(misc)
 
 
 BASE_URL = "http://localhost:8000"
@@ -31,22 +39,22 @@ print(resp.content.decode("utf-8"))
 # Vamos inserir um novo item no endpoint
 # Atenção: nas libs do JS e TS a gente usa o formato JSON normal.
 #          Aqui no requests, precisei adaptar pro padrão user.atributo.
-
-files = {}
 with open("chifre.jpg", "rb") as profile_pic:
     files = {
         "foto_perfil": profile_pic,
     }
+    senha = fake.password()
+    print(f"A senha é: {senha}")
     novo_corno = {
-        "user.username": "jao2",
-        "user.password": "jao1234!",
-        "user.email": "jao@cornos.com",
-        "user.first_name": "João",
-        "user.last_name": "dos Chifres Tortos",
+        "user.username": fake.user_name(),
+        "user.password": senha,
+        "user.email": fake.ascii_email(),
+        "user.first_name": fake.first_name(),
+        "user.last_name": fake.last_name(),
         "foto_perfil": "chifre.jpg",
-        "qtd_chifres": 10,
-        "ultimo_chifre": datetime.now(),
-        "procurando_mais": "true",
+        "qtd_chifres": 0,
+        "ultimo_chifre": fake.date_time(),
+        "procurando_mais": fake.boolean(),
     }
 
     resp = r.post(
